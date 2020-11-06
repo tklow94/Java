1d9d3488e4afc9f9a9d68af0b0c9e35cfae73d8d
Trevor <tlow94@uw.edu>
2020-11-05 17:45:00 -0800

    first commit

---
+++ notes.js
@@ -0,0 +1,3 @@
+//Java is a high order, OOP and easy to use cross-platform enterprise language.
+    //Java uses OOP: Abstraction, encapsulation, inheritance, and polymorphism.
+//class is a model for an object, it is the blueprint of the state and behavior(methods) We have states(attributes) with values. We also have methods. Objects act upon objects invoking different behaviors or methods.
